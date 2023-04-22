829e2653bd1bc258852c8effa548fe78e9c7fc66
vzhiv <vzhiv@fvis.ru>
2023-04-22 19:20:15 +0300

    add data

--- src/main/java/org/example/seminar01/task01/Main.java
+++ src/main/java/org/example/seminar01/task01/Main.java
@@ -1,19 +1,35 @@
-package org.example;
+package org.example.seminar01.task01;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите целое число: ");
+        int num = iScanner.nextInt();
+        iScanner.close();
+        System.out.println(subtractProductAndSum(num));
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    /**
+     *
+     * @param number = заданное число
+     * @return произведение цифр - сумма цифр
+     */
+    public static int subtractProductAndSum(int number) {
+
+        int sum = 0;
+        int mult = 1;
+        while (number > 0) {
+            int temp = number % 10;
+            sum += temp;
+            mult *= temp;
+            number /= 10;
         }
+        int result = mult - sum;
+        return result;
     }
 }
